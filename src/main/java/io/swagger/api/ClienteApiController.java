acbf03eddc8b4a9290b4260f318c805dec51c0a8
Victtor <victtorfreitas95@gmail.com>
2020-09-16 09:16:35 -0300

    Configura controller cliente

--- src/main/java/io/swagger/api/ClienteApiController.java
+++ src/main/java/io/swagger/api/ClienteApiController.java
@@ -1,10 +1,13 @@
 package io.swagger.api;
 
+import io.swagger.customizacao.service.ClienteService;
+import io.swagger.customizacao.util.RespostasUtil;
 import io.swagger.model.Cliente;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -25,30 +28,20 @@ import java.util.List;
 @Controller
 public class ClienteApiController implements ClienteApi {
 
-    private static final Logger log = LoggerFactory.getLogger(ClienteApiController.class);
+    @Autowired
+    private ClienteService clienteService;
 
-    private final ObjectMapper objectMapper;
-
-    private final HttpServletRequest request;
-
-    @org.springframework.beans.factory.annotation.Autowired
-    public ClienteApiController(ObjectMapper objectMapper, HttpServletRequest request) {
-        this.objectMapper = objectMapper;
-        this.request = request;
-    }
+    @Autowired
+    private RespostasUtil respostasUtil;
 
     public ResponseEntity<Cliente> cadastraCliente(@ApiParam(value = ""  )  @Valid @RequestBody Cliente cliente_) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<Cliente>(objectMapper.readValue("{  \"senha\" : \"senha123\",  \"cpf\" : \"01234567890\",  \"id\" : 234,  \"contas\" : [ {    \"numero\" : 784563,    \"saldo\" : 101.34,    \"agencia\" : 975,    \"digito\" : 7  }, {    \"numero\" : 784563,    \"saldo\" : 101.34,    \"agencia\" : 975,    \"digito\" : 7  } ],  \"titular\" : \"Janete Silva\"}", Cliente.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<Cliente>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
 
-        return new ResponseEntity<Cliente>(HttpStatus.NOT_IMPLEMENTED);
+        try {
+            return clienteService.salva(cliente_);
+        } catch (Exception e) {
+            return respostasUtil.getErroInternoCliente(ClienteService.FALHA_AO_TENTAR_CADASTRAR_UM_CLIENTE);
+        }
     }
+
 
 }
